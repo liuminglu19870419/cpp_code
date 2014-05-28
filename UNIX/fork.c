2fad5564c1b6f0fd708bd9f37de3986d57a25fac
mlliu <mlliu@mlliu-virtual-machine.(none)>
2014-05-28 23:37:56 +0800

    add process code

---
+++ UNIX/fork.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int global = 0;
+int main(int argc, char * argv[])
+{
+    int var;
+    pid_t pid;
+    char buf[] = "write to stdout\n";
+    var = 99;
+    if (write(STDOUT_FILENO, buf, sizeof(buf) - 1 ) != sizeof(buf) - 1)
+    {
+        exit(0);
+    }
+
+    pid = fork();
+    if(pid == 0)
+    {
+        var++;
+        global ++;
+    } else {
+        sleep(2);
+    }
+    printf("pid:%d var:%d global:%d\n", getpid(), global, var);
+}
